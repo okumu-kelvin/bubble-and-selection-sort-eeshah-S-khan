ee9c0466c2ce4226fb3cd6d434e82248be6968e1
eeshah-S-khan <eeshahsabirkhan144@gmail.com>
2025-06-20 13:43:59 +0300

    sorting assignment

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,11 @@
 def selection_sort(arr):
-    # TODO: Implement selection sort
-    pass
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        # minimum element in the unsorted part
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        # Swap minimum element with the first element of the unsorted part
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
